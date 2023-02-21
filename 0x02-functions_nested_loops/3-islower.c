f32a5e1c957ecce7852d2336646e7a75c6081166
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-02-21 11:35:48 -0800

    Cloops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,17 @@
 
 /**
  * _islower - check if char is lowercase
- * @c: isthe char to be checked
+ * @c: is the char to be checked
  * Return: 1if char is lowercase, otherwise 0.
  */
 
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	
-		return (1);
-	
-	else
-		return (0);
+if (c >= 'a' && c <= 'z')	
 
+return (1);	
+	
+else
+return (0);
 }
+
