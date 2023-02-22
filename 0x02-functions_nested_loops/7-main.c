34615eeffd27385c777594779bfe451723091334
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-02-21 17:17:49 -0800

    C loops

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
-
 /**
 * main - check the code
 *
 * Return: Always 0.
 */
+
 int main(void)                
 {        
-int r;
+
+	int r;
 print_last_digit(98);
 print_last_digit(0);
 r = print_last_digit(-1024);
