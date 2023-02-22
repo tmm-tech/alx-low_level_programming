1af22e25c58de3e423cfaed11b43ec4bf349e766
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-02-22 04:38:39 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,13 @@
  */
 int print_last_digit(int n)
 {
-int a;        
-if (n < 0)
-  n = -n;
-a = n % 10;
-if (a < 0)
-  a = -a;
-_putchar(a + '0');
+  int a;        
+
+  if (n < 0)
+    n = -n;
+  a = n % 10;
+  if (a < 0)
+    a = -a;
+  _putchar(a + '0');
 return (a);
 }
