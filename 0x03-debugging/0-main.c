3d1f7e01ddc379547a2312a1876729a4d5783254
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-02-23 16:25:36 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
-carrie@ubuntu:/debugging$ cat main.c
-#include "holberton.h"
+#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -8,10 +7,10 @@ carrie@ubuntu:/debugging$ cat main.c
 
 int main(void)
 {
-        int i;
+int i;
 
-        i = 98;
-        positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-        return (0);
+return (0);
 }
