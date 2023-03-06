ef211ba7f74ef04040eb04176b6d275f26b7feab
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-03-06 13:51:22 -0800

    Update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -27,6 +27,5 @@ char *_strpbrk(char *s, char *accept)
 
 		s++;
 	}
-	
 	return ('\0');
 }
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -29,7 +29,8 @@ char *_strstr(char *haystack, char *needle)
 		{
 			do {
 				if (needle[index + 1] == '\0')
-					return (haystack)
+					return (haystack);
+
 				index++;
 			} while (haystack[index] == needle[index]);
 
@@ -37,5 +38,6 @@ char *_strstr(char *haystack, char *needle)
 
 		haystack++;
 	}
+
 	return ('\0');
 }
