ec86502ec9930eb2bf7ac2137e1b2ee874531519
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-03-08 19:37:40 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,46 @@
 #include "main.h"
 
-int find_strlen(char *s);
+int _strlen_recursion(char *s);
 int check_palindrome(char *s, int len, int index);
-int is_palindrome(char *s);
-
 /**
- * find_strlen - Returns the length of a string.
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if t is 0 it's not
+ */
+int is_palindrome(char *s)
+{
+	if(*s == 0)
+		return(1);
+	return (check_palindrome(s, 0, _strlen_recursion(s));
+}
+/**
+ * _strlen_recursion  - Returns the length of a string.
  * @s: The string to be measured.
  *
  * Return: The length of the string.
  */
-int find_strlen(char *s)
+int _strlen_recursion(char *s)
 {
-	int len = 0;
+	if (*s == '\0')
+	
+		return (0);
 
-	if (*(s + len))
-	{
-		len++;
-		len += find_strlen(s + len);
-	}
-
-	return (len);
+	return (1 + _sterlen_recursion(s + 1);
 }
+/**
+ * check_palindrome - check the characters recursively for palindrome
+ * @s: string to check
+ * @i:iterator
+ * @len: lengthof the string
+ * 
+ *Return :1 if palindrome,0 if not
+ */
+int check_palindrome(char *s, int i, int len)
+{
+	if(*(s +1) != *(s + len -1))
+			return (0);
+	if(i >= len)
+		return (1);
+	return (check_palindrome(s,i+1,len 1, i +1 ,len-1));
+)
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,6 +3,19 @@
 int is_divisible(int num, int div);
 int is_prime_number(int n);
 /**
+ * is_prime_number - says if an integer is a prime number
+ * or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is aprime number, 0 ifnot
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_divisible(n, n - 1));
+}
+/**
  * is_divisible - Checks if a number is divisible.
  * @num: The number to be checked.
  * @div: The divisor.
@@ -12,6 +25,9 @@ int is_prime_number(int n);
  */
 int is_divisible(int num, int div)
 {
-	if (num % div == 0)
+	if (div == 1)
+		return (1);
+	if (num % div == 0 && div > 0)
 		return (0);
-	
+	return (is_divisible(num, div - 1));
+}
