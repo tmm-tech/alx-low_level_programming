1a4e0f5d231c7ac6a1b006283c9012f1ed7febeb
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-03-09 07:09:14 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -75,14 +75,21 @@ char *postfix_match(char *str, char *postfix)
  */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s2 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
 	if (*s2 == '*')
 	{
-		iterate_wild(&s2);
-		s2 = postfix_match(s1, s2);
+		return ((wildcmp(s1 + 1, s2) || (wildcmp(s1, s2 + 1));
+	}	
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (*s2 == '\0')
-		return (1);
-	if (*s1 != *s2)
-		return (0);
-	return (wildcmp(++s1, ++s2));
+	return (0);
 }
