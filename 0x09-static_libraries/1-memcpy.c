2f97827aafe6e6ad99ef54a234a0db0155cd424c
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-03-11 00:03:11 +0300

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,41 +1,23 @@
-
-#include "main.h"
+#include "holberton.h"
 
 /**
-
- *_memcpy - a function that copies memory area
-
- *@dest: memory where is stored
-
- *@src: memory where is copied
-
- *@n: number of bytes
-
+ * _memcpy - This is my funcion copy Src to dest
+ * @dest: This is my result
+ * @src: This is my string
+ * @n: This is n my number bytes to copy
  *
-
- *Return: copied memory with n byted changed
-
+ * Return: This my my string copy
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
-
 {
+	unsigned int a;
+	char *pDst = dest;
+	char const *pSrc =  src;
 
-        int r = 0;
-
-        int i = n;
-
-
-        for (; r < i; r++)
-
-        {
-
-                dest[r] = src[r];
-
-                n--;
-
-        }
-
-        return (dest);
-
+	for (a = 0; a < n; a++)
+	{
+		*pDst++ = *pSrc++;
+	}
+	return (dest);
 }
