f3cf1bfae0be442682f1db2df9f036414d9b3086
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-03-10 20:29:02 -0800

    tasks

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,25 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
-
  * _abs - computes the absolute value of an integer
-
  * @n: the int to check
-
  * Return: the absolute value of int
-
  */
-
 int _abs(int n)
-
 {
-
-        if (n >= 0)
-
-        {
-
-                return (n);
-
-        }
-
-        return (-n);
-
+	return abs(n);
 }
