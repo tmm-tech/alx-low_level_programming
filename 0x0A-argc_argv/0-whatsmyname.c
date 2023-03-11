a5b5bbde4fa9bb9e976275bf45fc15c42cfe939b
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-03-10 20:16:36 -0800

    Tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
-/*8
+/*
  * main - Print the name of the program
  * @argc: Count arguments
  * @argv:  Arguments
@@ -11,6 +11,6 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	(void) argv; 
-	printf("%i\n",argc-1);
+	(void) argv;
+	printf("%i\n", argc - 1);
 	return (0);
 }
