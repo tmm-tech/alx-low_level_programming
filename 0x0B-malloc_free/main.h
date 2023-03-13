8fd868aadb2021994cf3435e6a2faa5e0bcb5005
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-03-13 06:12:35 -0700

    Secondtask

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
-int _putcar(char c);
+
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *strdup(char *str);
 char *str_concat(char *s1, char *s2);
