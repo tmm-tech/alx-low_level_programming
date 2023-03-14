badc65bf45af7d727c9e3350097af545c59480ef
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-03-14 07:24:25 +0300

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+*_strncpy - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
   return strncpy(dest, src, n);
