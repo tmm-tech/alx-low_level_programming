9bae6cb6e3f6c8b5bd9692a471fd61b81857a7cb
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-03-14 07:25:42 +0300

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+*_putchar - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
 int _putchar(char c)
 {
   return putchar(c);
